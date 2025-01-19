12082a4161a0d7a251f0826fa9d298760f895026
JuliaSolodkaya <117906617+JuliaSolodkaya@users.noreply.github.com>
2025-01-19 20:53:13 +1000

    Update test.java

--- test.java
+++ test.java
@@ -4,6 +4,8 @@ public class test {
         System.out.println("hello world!!!!");
         System.out.println("hello world!!!!");
         System.out.println("hello world!!!!");
+        System.out.println("hello world!!!!");
+        System.out.println("hello world!!!!");
 
     }
 }
